9329f4a5e8a8eaff90b5093beffcbeb1c0b516ef
Abubakr <Abubakr@AbubakrPC>
2021-03-04 18:11:41 +0000

    I have created an empty 750x250 Window for a Start Menu

--- Project2(Group 52)/src/StartUp.java
+++ Project2(Group 52)/src/StartUp.java
@@ -1,9 +1,24 @@
-
+import javax.swing.*;
 public class StartUp {
-
+	static JFrame startUpFrame = new JFrame();
+	static JPanel startUpPanel = new JPanel(null);
+	
+public StartUp() { //setting up StartUp Frame and Panel using Constructor Method
+		
+		startUpFrame.setTitle("Start Menu");
+		startUpFrame.setSize(750,250);
+		startUpFrame.setResizable(false);
+		//setUndecorated(true);
+	    //getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+	    
+		startUpFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		startUpFrame.add(startUpPanel);
+		startUpFrame.setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Commit #1");
+		StartUp startUpGui = new StartUp();
 	}
 
 }
