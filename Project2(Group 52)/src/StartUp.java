4304ebb5a5c0647747a62a9ca2494b71cf4a3410
Abubakr <Abubakr@AbubakrPC>
2021-03-04 20:18:55 +0000

    The TopBar(Header) size has been slightly increased

--- Project2(Group 52)/src/StartUp.java
+++ Project2(Group 52)/src/StartUp.java
@@ -7,7 +7,6 @@ public class StartUp {
 	static JLabel topBar = new JLabel("Inventory Management System");
 	Color topFontColor = new Color(255,255,255);
 	Font topBarFont = new Font("Serif",Font.PLAIN,36);
-	
 public StartUp() { //setting up StartUp Frame and Panel using Constructor Method
 		
 		startUpFrame.setTitle("Start Menu");
@@ -18,11 +17,13 @@ public StartUp() { //setting up StartUp Frame and Panel using Constructor Method
 	    
 		startUpFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
-		topBar.setBounds(1,1,750,20);
+		topBar.setBounds(1,1,750,30);
 		startUpPanel.add(topBar);
 		topBar.setOpaque(true);
 		topBar.setBackground(Color.BLACK); //Black Background
 		topBar.setForeground(topFontColor); //White Text
+		
+		
 		startUpFrame.add(startUpPanel);
 		startUpFrame.setVisible(true);
 	}
